a777ce111ca5ba28b8dd30a55226ff045c0216cf
glacasse <glacasse@e9d1ed95-6609-1d95-0ca3-e06bebb388b0>
2010-03-20 15:50:07 +0000

    Added first files to project

---
+++ trunk/Linguistics/StringManipulations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linguistics
+{
+    /// <summary>
+    /// Used for generic string manipulations
+    /// </summary>
+    static class StringManipulations
+    {
+        /// <summary>
+        /// Replace content in string but keep original case
+        /// </summary>
+        /// <param name="original">original</param>
+        /// <param name="from">to replace</param>
+        /// <param name="to">to replace to</param>
+        /// <returns>String with replaced content with case kept</returns>
+        public static string ReplaceKeepCase(this string original, string from, string to)
+        {
+        }
+    }
+}
