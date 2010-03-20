920164348961060abeb8d65a54cb7b8c2ac5f82f
glacasse <glacasse@e9d1ed95-6609-1d95-0ca3-e06bebb388b0>
2010-03-20 15:50:07 +0000

    Added first files to project

---
+++ Linguistics/Transformations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linguistics
+{
+    /// <summary>
+    /// Used for linguistic transformations
+    /// </summary>
+    static class Transformations
+    {
+        /// <summary>
+        /// Invert "YOU" and "I" from string (your and my etc...)
+        /// </summary>
+        /// <param name="originalText">original text</param>
+        /// <returns>Text with YOU and I inverted (your and my etc...)</returns>
+        public static string InvertFirstSecondPerson(string originalText)
+        {
+        }
+    }
+}
---
+++ UnitTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Linguistics;
+
+namespace RightBrain
+{
+    static class UnitTests
+    {
+        #region Public Methods
+        public static void TestAll()
+        {
+            TestStringManipulations();
+            TestLinguisticsTransformations();
+        }
+        #endregion
+
+        #region Private Methods
+        private static void TestStringManipulations()
+        {
+            AssertEquals("Better is better".ReplaceKeepCase("better", "worse"), "Worse is worse");
+        }
+
+        private static void TestLinguisticsTransformations()
+        {
+            AssertEquals(Transformations.InvertFirstSecondPerson("I love you"), "You love me");
+            AssertEquals(Transformations.InvertFirstSecondPerson("You love me"), "I love you");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("I listen to you"), "You listen to me");
+            AssertEquals(Transformations.InvertFirstSecondPerson("You listen to me"), "I listen to you");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("This is my hat"), "This is your hat");
+            AssertEquals(Transformations.InvertFirstSecondPerson("This is your hat"), "This is my hat");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("I'm the best"), "You're the best");
+            AssertEquals(Transformations.InvertFirstSecondPerson("You're the best"), "I'm the best");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("I am the best"), "You are the best");
+            AssertEquals(Transformations.InvertFirstSecondPerson("You are the best"), "I am the best");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("You will be rewarded"), "I will be rewarded");
+            AssertEquals(Transformations.InvertFirstSecondPerson("I will be rewarded"), "You will be rewarded");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("You'll be rewarded"), "I'll be rewarded");
+            AssertEquals(Transformations.InvertFirstSecondPerson("I'll be rewarded"), "You'll be rewarded");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("This hat is yours"), "This hat is mine");
+            AssertEquals(Transformations.InvertFirstSecondPerson("This hat is mine"), "This hat is yours");
+
+            AssertEquals(Transformations.InvertFirstSecondPerson("You will do it by yourself"), "I will do it by myself");
+            AssertEquals(Transformations.InvertFirstSecondPerson("I will do it by myself"), "You will do it by yourself");
+        }
+
+        private static void AssertEquals(string string1, string string2)
+        {
+            if (string1 != string2)
+                throw new Exception("Wrong value: "+string2 +" should equals: "+string1);
+        }
+        #endregion
+    }
+}
