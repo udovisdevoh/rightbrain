02d31659a8d263cac7909ef3e659ef90cde58206
glacasse <glacasse@e9d1ed95-6609-1d95-0ca3-e06bebb388b0>
2010-03-20 19:01:09 +0000

    Added stub for firstSecondPerson manager

---
+++ trunk/Linguistics/Transformations/Transformations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linguistics
+{
+    /// <summary>
+    /// Used for linguistic transformations
+    /// </summary>
+    static class Transformations
+    {
+        #region Parts
+        /// <summary>
+        /// Manages words such as you, me, I, yours mine, my, mines etc...
+        /// </summary>
+        private static FirstSecondPersonManager firstSecondPersonManager = new FirstSecondPersonManager();
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Invert "YOU" and "I" from string (your and my etc...)
+        /// </summary>
+        /// <param name="originalText">original text</param>
+        /// <returns>Text with YOU and I inverted (your and my etc...)</returns>
+        public static string InvertFirstSecondPerson(string originalText)
+        {
+            return firstSecondPersonManager.InvertFirstSecondPerson(originalText);
+        }
+        #endregion
+    }
+}
