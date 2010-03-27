b8ff58006cdae85888e4c0d31d432f6f17e84912
glacasse <glacasse@e9d1ed95-6609-1d95-0ca3-e06bebb388b0>
2010-03-27 19:26:00 +0000

    Added stub for question detection Added unit tests for question detection

---
+++ trunk/Linguistics/Transformations/Parts/Questions/QuestionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linguistics
+{
+    /// <summary>
+    /// Manages operations and analysis on questions
+    /// </summary>
+    internal class QuestionManager
+    {
+        #region Parts
+        /// <summary>
+        /// Manages questions that are questions just because they contain ? for they wouldn't be questions otherwise
+        /// </summary>
+        private QuestionManagerByMarkOnly questionManagerByMarkOnly = new QuestionManagerByMarkOnly();
+
+        /// <summary>
+        /// Manages questions that are questions because they start with question starting words
+        /// </summary>
+        private QuestionManagerByStartingWithQuestionWord questionManagerByStartingWithQuestionWord = new QuestionManagerByStartingWithQuestionWord();
+        #endregion
+
+        #region Internal Methods
+        /// <summary>
+        /// Whether provied proposition is question or not
+        /// </summary>
+        /// <param name="originalProposition">provided proposition</param>
+        /// <returns>Whether provied proposition is question or not</returns>
+        internal bool IsQuestion(string originalProposition)
+        {
+            if (questionManagerByStartingWithQuestionWord.IsQuestion(originalProposition))
+                return true;
+            else if (questionManagerByMarkOnly.IsQuestion(originalProposition))
+                return true;
+
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ trunk/Linguistics/Transformations/Parts/Questions/questionTypes/QuestionManagerByMarkOnly.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linguistics
+{
+    /// <summary>
+    /// Manages questions that are questions just because they contain ? for they wouldn't be questions otherwise
+    /// </summary>
+    internal class QuestionManagerByMarkOnly
+    {
+        #region Internal Methods
+        /// <summary>
+        /// Whether question was detected by ?
+        /// </summary>
+        /// <param name="originalProposition">original proposition</param>
+        /// <returns>Whether question was detected by ?</returns>
+        internal bool IsQuestion(string originalProposition)
+        {
+            return originalProposition.Contains('?');
+        }
+        #endregion
+    }
+}
